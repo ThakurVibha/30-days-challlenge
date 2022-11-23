c77a24e206da1951f0c8be053dfcee30b37a094e
Vinen Glasswala <vinen.g@cearsinfotech.com>
2022-11-23 20:29:22 +0530

    ---->>>understood basic structure of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const  name=<h1>Vibha Thakur</h1>
+  const age=<h2>25</h2>
+  const user = <div className="App">
+  {name}
+  {age}
+    </div>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {user}
+    {user}
+    {user}
+    {user}
+
+    {user}
+
+    {user}
+
+
+
+      </div>
   );
 }
 
